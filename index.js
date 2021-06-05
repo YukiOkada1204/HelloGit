68b50bf7c1ba5f506012528c2c23e7cec1390139
YukiOkada1204 <okadayuki@okadayuukinoMacBook-Air.local>
2021-06-05 14:58:29 +0900

    Alert

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function addMoney(added){
     money += added
     document.getElementById('moneyarea').textContent = money;
 }
+
+const alert = () => {
+    if (parseInt(document.getElementById('moneyarea').textContent) >= 500){
+        console.log(`現在${document.getElementById('moneyarea').textContent}円です。`);
+    }
+}
+
+const watcher = setInterval(alert, 1000);
